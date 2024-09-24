997ecca301330396a95a1595817dcce46c06e836
sonndc <tuyenlv.b19vt361@stu.ptit.edu.vn>
2024-09-24 17:18:34 +0700

    tuyen.laivan
    connect mysql

--- src/main/java/com/vti/tuyn/identityservicedemo/configuration/ApplicationInitConfig.java
+++ src/main/java/com/vti/tuyn/identityservicedemo/configuration/ApplicationInitConfig.java
@@ -36,7 +36,7 @@ public class ApplicationInitConfig {
     @ConditionalOnProperty(
             prefix = "spring",
             value = "datasource.driverClassName",
-            havingValue = "org.postgresql.Driver")
+            havingValue = "com.mysql.cj.jdbc.Driver")
     ApplicationRunner applicationRunner(UserRepository userRepository, RoleRepository roleRepository) {
         log.info("Initializing application.....");
         return args -> {
